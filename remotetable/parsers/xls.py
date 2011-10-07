168afeb830902234e807895646bbd18cce231346
Jacob Kaplan-Moss <jacob@jacobian.org>
2011-10-07 17:41:23 -0500

    Refactored XLS/X parsers to use a common base class.

--- remotetable/parsers/xls.py
+++ remotetable/parsers/xls.py
@@ -1,21 +1,15 @@
 import xlrd
 
-class Parser(object):
-
+class BaseXLSParser(object):
     def __init__(self, stream, **options):
         self.stream = stream
         self.options = options
 
     def read(self):
-        # Gross: xlrd can't read streams.
-        workbook = xlrd.open_workbook(file_contents=self.stream.read())
+        workbook = self.open_workbook()
+        worksheet = self.get_worksheet(workbook)
 
-        if 'sheet' in self.options:
-            worksheet = workbook.sheet_by_name(self.options['sheet'])
-        else:
-            worksheet = workbook.sheet_by_index(0)
-
-        data = [worksheet.row_values(i) for i in range(worksheet.nrows)]
+        data = self.extract_data(worksheet)
         data = data[self.options.get('skip', 0):]
 
         headers = self.options.get('headers', True)
@@ -25,3 +19,25 @@ class Parser(object):
             return [dict(zip(headers, row)) for row in data]
         else:
             return data
+
+    def get_worksheet(self, workbook):
+        sheet_name = self.options.get('sheet', 0)
+        try:
+            worksheet = self.get_sheet_by_index(workbook, int(sheet_name))
+        except ValueError:
+            worksheet = self.get_sheet_by_name(workbook, sheet_name)
+        return worksheet
+
+class Parser(BaseXLSParser):
+    def open_workbook(self):
+        # Gross: xlrd can't read streams.
+        return xlrd.open_workbook(file_contents=self.stream.read())
+
+    def get_sheet_by_index(self, workbook, index):
+        return workbook.sheet_by_index(index)
+
+    def get_sheet_by_name(self, workbook, name):
+        return workbook.sheet_by_name(name)
+
+    def extract_data(self, worksheet):
+        return [worksheet.row_values(i) for i in range(worksheet.nrows)]
--- remotetable/parsers/xlsx.py
+++ remotetable/parsers/xlsx.py
@@ -1,31 +1,21 @@
+from __future__ import absolute_import
+
 from cStringIO import StringIO
 from openpyxl.worksheet import flatten
 from openpyxl.reader.excel import load_workbook
+from .xls import BaseXLSParser
 
-class Parser(object):
-
-    def __init__(self, stream, **options):
-        self.stream = stream
-        self.options = options
-
-    def read(self):
+class Parser(BaseXLSParser):
+    def open_workbook(self):
         # Unfortunate memory-hogging hack to work around zipfile not being
         # able to open urllib streams directly.
-        stream = StringIO(self.stream.read())
-        workbook = load_workbook(stream)
+        return load_workbook(StringIO(self.stream.read()))
 
-        if 'sheet' in self.options:
-            worksheet = workbook.get_sheet_by_name(self.options['sheet'])
-        else:
-            worksheet = workbook.worksheets[0]
+    def get_sheet_by_index(self, workbook, index):
+        return workbook.worksheets[index]
 
-        data = flatten(worksheet.rows)
-        data = data[self.options.get('skip', 0):]
+    def get_sheet_by_name(self, workbook, name):
+        return workbook.get_sheet_by_name(name)
 
-        headers = self.options.get('headers', True)
-        if headers is True:
-            headers, data = data[0], data[1:]
-        if headers:
-            return [dict(zip(headers, row)) for row in data]
-        else:
-            return data
+    def extract_data(self, worksheet):
+        return flatten(worksheet.rows)
